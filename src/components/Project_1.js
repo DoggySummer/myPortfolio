a2146db2effccb8e8661b40dec8b9722e1262788
DoggySummer <91588779+DoggySummer@users.noreply.github.com>
2023-03-14 15:54:41 +0900

    asd

--- src/components/Project_1.js
+++ src/components/Project_1.js
@@ -65,7 +65,7 @@ const Project_1 = () => {
               {/* <Swiper
                 className="swiper_container"
                 modules={[Navigation, Pagination, Scrollbar, A11y]}
-                spaceBetween={500}
+                spaceBetween={5}
                 slidesPerView={3}
                 pagination={{ clickable: true }}
                 navigation={true}
